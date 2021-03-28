86fe505ef0fbff13d4cc39eb7b9d99c0b156dbc1
Thiago Araujo <thiago.araujotga@outlook.com>
2021-03-28 17:39:19 -0300

    iniciando contato page

--- my-app/src/Componentes/FormContato/FormContato.jsx
+++ my-app/src/Componentes/FormContato/FormContato.jsx
@@ -1,37 +1,38 @@
-import React, { Component } from "react";
-import "./FormContato.css";
+import React from 'react';
+import './FormContato.css';
+import imgForm from  '../../Asserts/img/contact-pic.jpg'
 
-export default class FormContato extends Component {
-  render() {
-    return (
-      <div className="Form-contato content">
-        <h1 className="title-contato">Contato</h1>
-        <img className="contact-pic" src="../img/contact-pic.jpg" alt="" />
-        <div className="form">
-          <form className="contact-form">
-            <div className="item-form">
-              <label htmlFor="name">Nome: </label>
-              <input type="text" name="name" id="" />
-            </div>
-            <div className="item-form">
-              <label htmlFor="email">E-mail: </label>
-              <input type="email" name="email" id="" />
-            </div>
-            <div className="item-form">
-              <label htmlFor="message">Mensagem: </label>
-              <textarea name="message"></textarea>
-            </div>
-            <div className="item-form">
-              <button
-                className="btn-contact highlight-secondary "
-                type="submit"
-              >
-                Enviar
-              </button>
-            </div>
-          </form>
-        </div>
+
+const FormContato = () => {
+  return (
+      <div className="Form-contato-content">
+      <h1 className="title-contato">Contato</h1>
+      <img className="contact-pic" src={imgForm} alt="" />
+      <div className="form">
+        <form className="contact-form">
+          <div className="item-form">
+            <label htmlFor="name">Nome: </label>
+            <input type="text" name="name" id="" />
+          </div>
+          <div className="item-form">
+            <label htmlFor="email">E-mail: </label>
+            <input type="email" name="email" id="" />
+          </div>
+          <div className="item-form">
+            <label htmlFor="message">Mensagem: </label>
+            <textarea name="message"></textarea>
+          </div>
+          <div className="item-form">
+            <button
+              className="btn-contact highlight-secondary "
+              type="submit"
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  )
 }
+export default FormContato
---
+++ my-app/src/Pages/Contato/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import FormContato from '../../Componentes/FormContato/FormContato'
+const Contato = () => {
+    return (
+        <div>
+           <FormContato/>
+        </div>
+    )
+}
+
+export default Contato
